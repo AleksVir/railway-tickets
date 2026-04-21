c01676290f19a9d8c64f27e192db59b764cef2cb
Aleksandra Vir <galex97@mail.ru>
2026-04-22 00:20:51 +0300

    setup1

--- eslint.config.js
+++ eslint.config.js
@@ -1,54 +1,23 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
-import { fixupConfigRules } from '@eslint/compat';
-import tsParser from '@typescript-eslint/parser';
-import globals from 'globals';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['**/dist']),
+  globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: fixupConfigRules(compat.extends(
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react-hooks/recommended'
-    )),
-    settings: {
-      react: {
-        version: 'detect'
-      }
-    },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
-      parser: tsParser,
       ecmaVersion: 2020,
-      sourceType: 'module',
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true
-        }
-      },
-      globals: {
-        ...globals.browser
-      }
+      globals: globals.browser,
     },
-    rules: {
-      'react/prop-types': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-no-target-blank': ['error', { allowReferrer: false }]
-    }
-  }
-]);
+  },
+])
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    eslint({
-      include: ['src/**/*.{ts,tsx,js,jsx}'], 
-      exclude: ['node_modules', 'dist'], 
-      fix: false,
-      cache: true, 
-      emitError: true, 
-      emitWarning: true 
-    })
-  ]
-});
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+})
